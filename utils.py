7baaf60b0ebb923653ef4ec61e256cd174ddc0c2
summermyxia <summermyxia@icloud.com>
2020-06-24 11:23:49 -0400

    Revise Turnbull estimator

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def turnbull(data, interval_length = 3, low_price = 0, high_price = -1, num_bins
     
     intervals = []
     for price, win in data:
-        intervals.append((price, min(price + interval_length, high_price)) if win else (max(price - interval_length, low_price), price))
+        intervals.append((max(price - interval_length, low_price), price) if win else (price, min(price + interval_length, high_price)))
     
     bins = [low_price]
     bin_width = (high_price - low_price) / num_bins
@@ -16,28 +16,37 @@ def turnbull(data, interval_length = 3, low_price = 0, high_price = -1, num_bins
     
     alpha = []
     for left, right in intervals:
-        new_alpha_row = [0 for _ in range(num_bins)]
+        # new_alpha_row = [0 for _ in range(num_bins)]
         left_idx = bisect.bisect_left(bins, left)
         right_idx = bisect.bisect_left(bins, right)
-        new_alpha_row[left_idx:(right_idx + 1)] = [1] * (right_idx - left_idx + 1)
-        alpha.append(new_alpha_row)
+        alpha.append([left_idx, right_idx])
+        # new_alpha_row[left_idx:(right_idx + 1)] = [1] * (right_idx - left_idx + 1)
+        # alpha.append(new_alpha_row)
 
     density = [1 / num_bins for _ in range(num_bins)]
     diff = float('inf')
-    n, m = len(alpha), len(alpha[0])
+    n, m = len(data), num_bins
     while diff > epsilon:
         denominators = []
         for i in range(n):
-            denominators.append(sum([alpha[i][j] * density[j] for j in range(m)]))
+            # denominators.append(sum([alpha[i][j] * density[j] for j in range(m)]))
+            denominators.append(sum([density[j] for j in range(alpha[i][0], alpha[i][1] + 1)]))
         
-        newdensity = []
+        # newdensity = []
+        newdensity = [0] * num_bins
+        for i in range(n):
+            if denominators[i] != 0:
+                for j in range(alpha[i][0], alpha[i][1] + 1):
+                    newdensity[j] += density[j] / denominators[i]
         for j in range(m):
-            value = 0
-            for i in range(n):
-                if denominators[i] != 0:
-                    value += alpha[i][j] * density[j] / denominators[i]
-            value /= n
-            newdensity.append(value)
+            newdensity[j] /= n
+        # for j in range(m):
+        #     value = 0
+        #     for i in range(n):
+        #         if denominators[i] != 0:
+        #             value += alpha[i][j] * density[j] / denominators[i]
+        #     value /= n
+        #     newdensity.append(value)
             # newdensity.append(sum([(alpha[i][j] * density[j] / denominators[i]) if denominators[i] != 0 else 0 for i in range(n)]) / n)
 
         diff = sum([abs(newdensity[j] - density[j]) for j in range(m)])
